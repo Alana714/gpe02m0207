e8069c2384cd1405f4cf833497b7ebe32f6649b9
Alana714 <142412727+Alana714@users.noreply.github.com>
2023-10-04 19:59:01 -0300

    Update ativ10.c

--- atividadeAvaliativa/ativ10.c
+++ atividadeAvaliativa/ativ10.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include<string.h>
 
 main(){
 
-    int numCidades = 200, maiorIndice = -1, menorIndice = 999, totalVeiculos = 0, totalAcidentes = 0, totalCidadesRS = 0;
+    int numCidades = 2, totalVeiculos = 0, totalAcidentes = 0, totalCidadesRS = 0;
     char cidadeMaiorIndice[50], cidadeMenorIndice[50];
-    float mediaVeiculos, mediaAcidentesRS;
+    float mediaVeiculos, mediaAcidentesRS, maiorIndice = -1, menorIndice = 999;
 
     for(int i = 0; i < numCidades; i++){
         char estado[2];
@@ -26,28 +27,32 @@ main(){
         totalVeiculos += veiculos;
         totalAcidentes += acidentes;
 
-        if (estado[0] == 'R' && estado[1] == 'S') {
+        if (strcmp(estado, "RS") == 0) {
             totalCidadesRS++;
             mediaAcidentesRS += acidentes;
         }
 
         float indice = (float)acidentes / veiculos;
-        if (indice > (float)maiorIndice) {
+        if (indice > maiorIndice) {
             maiorIndice = indice;
             strcpy(cidadeMaiorIndice, estado);
         }
 
-        if (indice < (float)menorIndice) {
+        if (indice < menorIndice) {
             menorIndice = indice;
             strcpy(cidadeMenorIndice, estado);
         }
     }
 
-    mediaVeiculos = (float)totalVeiculos / numCidades;
-    mediaAcidentesRS /= totalCidadesRS;
+    if (totalCidadesRS > 0) {
+        mediaAcidentesRS /= totalCidadesRS;
+    }
 
-    printf("Maior indice de acidentes: %f (Cidade: %s)\n", maiorIndice, cidadeMaiorIndice);
-    printf("Menor indice de acidentes: %f (Cidade: %s)\n", menorIndice, cidadeMenorIndice);
+    mediaVeiculos = (float)totalVeiculos / numCidades;
+    
+
+    printf("Maior indice de acidentes: %.2f (Cidade: %s)\n", maiorIndice, cidadeMaiorIndice);
+    printf("Menor indice de acidentes: %.2f (Cidade: %s)\n", menorIndice, cidadeMenorIndice);
     printf("Media de veiculos nas cidades: %.2f\n", mediaVeiculos);
     printf("Media de acidentes com vitimas nas cidades do Rio Grande do Sul: %.2f\n", mediaAcidentesRS);
 
