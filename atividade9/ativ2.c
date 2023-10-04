7d4f7c4427aa67e374c419f3d61c35197d14d653
Alana714 <142412727+Alana714@users.noreply.github.com>
2023-10-04 19:36:44 -0300

    Update ativ2.c

--- atividade9/ativ2.c
+++ atividade9/ativ2.c
@@ -2,5 +2,15 @@
 
 main(){
 
-    int a{10}, 
+    int valores[6];
+
+    for(int i = 0; i < 6; i++){
+        printf("Digite os valores: ");
+        scanf("%d", &valores[i]);
+    }
+
+    for (int i = 0; i < 6; i++) {
+        printf("Valores lidos:%d\n", valores[i]);
+    }
+    
 }
